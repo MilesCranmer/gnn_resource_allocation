585416b2dd5722d0780ab77f279370eace1a4eb9
MilesCranmer <miles.cranmer@gmail.com>
2021-06-24 01:22:44 -0700

    Add file for generating graphs

---
+++ halos_to_graph.py
@@ -0,0 +1,24 @@
+"""Convert a halo catalog into a graph"""
+import torch
+import sys
+from torch.functional import F
+from torch import optim, nn
+from torch.utils.data import DataLoader, random_split, TensorDataset
+from torch import nn, optim
+from data import get_dataloader
+import pickle as pkl
+
+run_num = int(sys.argv[1])
+
+dataloader = get_dataloader(
+    [run_num], cols=["x", "y", "z", "M14", "redshift"], subsample=1e-2
+)
+
+graph = dataloader.dataset[0]
+pkl.dump(
+    graph,
+    open(
+        f"/mnt/home/mcranmer/ceph/general_data/gnn_allocation/graph_obj_large_{run_num}.pkl",
+        "wb",
+    ),
+)
